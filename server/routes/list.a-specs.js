c3b0e672d41f339b7201ab614d6ad8e5d2a55343
Victor Vilela <vilela_victor@hotmail.com>
2018-04-06 10:55:50 -0500

    add first acceptance test

---
+++ server/routes/list.a-specs.js
@@ -0,0 +1,32 @@
+
+var uuid = require('uuid');
+var axios = require('axios');
+var config = require('../../server/config/config')['test'];
+
+describe('/api', function(){
+    'use strict';
+    var userId;
+    before(function(){
+        userId = uuid.v4();
+    });
+
+    describe('list',function(){
+        context('When the user does NOT have a list', function(){
+            var actual;
+            beforeEach(function(){
+                var url = `http://localhost:${config.port}/api/list`;
+                var headers = { 
+                    Cookie: `${config.userCookieName}=${userId}`
+                };
+                return axios.get(url,{ headers })
+                    .then(function(response){
+                        actual = response.data;
+                    });    
+            });
+
+            it('should not return a list',function(){
+                expect(actual).to.be.null;
+            });
+        });
+    });
+});
